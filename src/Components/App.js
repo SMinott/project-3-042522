d62f11a6d098764c298a6a3962d7ebebb4a30c26
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-06-22 14:31:00 -0400

    added updated grocery.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,79 +1,39 @@
-import React, { useState, useEffect } from "react";
-import NewGroceryForm from "./NewGroceryForm";
-import GroceryList from "./GroceryList";
+import React, {useState} from "react";
 
+function Grocery({ grocery, handleDeleteGrocery }) {
+    const { id, name, category } = grocery;
+    const [ isDone, setIsDone ] = useState(true)
+//   const handleClick = () => {
+//     onDeleteGrocery(name)
+//   }
 
-function App() {
-  const [groceryList, setGroceryList] = useState([])
+    const handleClick = () => {
+        setIsDone(isDone => !isDone)
+    }
 
-  useEffect(() => {
-    fetch("http://localhost:9292/groceries")
-      .then((r) => r.json())
-      .then((groceryList) => setGroceryList(groceryList));
-  }, []);
-
-  const handleAddGrocery = (newGrocery) => {
-    setGroceryList([...groceryList, newGrocery])
+  const handleDelete= () => {
+      fetch(`http://localhost:9292/groceries/${id}`, {
+          method: "DELETE"
+      })
+      handleDeleteGrocery(id)
   }
-
-  const handleDeleteGrocery = (groceryToDelete) => {
-    setGroceryList(groceryList.filter((grocery) => grocery.id !== groceryToDelete))
-  }
-
-
-  const[show, setShow] = useState(true)
-
-  function handleClick() {
-    setShow(show => !show)
-  }
-  // const groceryListToDisplay = groceryList.filter((grocery) => grocery.category === category)
-  // // const groceryListToDisplay = groceryList.filter((grocery) => category === "All" || grocery.category === category)
-
-  
-
 
   return (
-    <div className="app">
-    <div className="sidebar">
-      <button onClick = {handleClick}>Grocery List</button>
-      {show ? <NewGroceryForm handleAddGrocery={handleAddGrocery} /> : null}
-    </div>
-        <GroceryList groceryList={groceryList} handleDeleteGrocery={handleDeleteGrocery}/>
-      </div>
-    );
-  }
+    // <div className="grocery">
+    //   <div className="label">{category}</div>
+    //   <div className="name">{name}</div>
+    //   <button className="delete" onClick={handleDelete}>🗑</button>
+    // </div>
 
+    <div>
+    <h3>{category}</h3>
+    <p>{name}</p>
+    <button onClick={handleClick}>
+      { isDone ? "🗸" : "☑"}
+      </button>
+      <button className="delete" onClick={handleDelete}>🗑</button>
+  </div>
+  );
+}
 
-//     <div className="App">
-//       <h2>My groceryList</h2>
-//       <CategoryFilter 
-//         categories={category}
-//         selectedCategory={category}
-//         onSelectCategory={setCategory}
-//       />
-//       <NewGroceryForm 
-//         categories={category.filter((cat) => cat)}
-//         // categories={category.filter((cat) => cat !== "All")}
-//         // categories={category}
-//         onGroceryFormSubmit={handleAddGrocery}
-//       />
-//       <GroceryList groceryList={groceryListToDisplay} onDeleteGrocery={handleDeleteGrocery}/>
-//     </div>
-//   );
-// }
-
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-// import { CATEGORIES, groceryList } from "../data";
-// console.log("Here's the data you're working with");
-// console.log({ CATEGORIES, groceryList });
+export default Grocery;
