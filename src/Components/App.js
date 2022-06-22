695824094743dcba1bd3c6e62f203c1e197d79ad
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-06-22 14:30:01 -0400

    added app.js update

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,35 +1,66 @@
-import './App.css';
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
-import GroceryCategory from './GroceryList';
-import GroceryContainer from './CategoryFilter';
-import Form from './NewGroceryForm'
+import React, { useState, useEffect } from "react";
+import NewGroceryForm from "./NewGroceryForm";
+import GroceryList from "./GroceryList";
 
 
 function App() {
-  const[ list, setlist] = useState([])
+  const [groceryList, setGroceryList] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/groceries")
+      .then((r) => r.json())
+      .then((groceryList) => setGroceryList(groceryList));
+  }, []);
+
+  const handleAddGrocery = (newGrocery) => {
+    setGroceryList([...groceryList, newGrocery])
+  }
+
+  const handleDeleteGrocery = (groceryToDelete) => {
+    setGroceryList(groceryList.filter((grocery) => grocery.id !== groceryToDelete))
+  }
+
+
+  const[show, setShow] = useState(true)
+
+  function handleClick() {
+    setShow(show => !show)
+  }
+  // const groceryListToDisplay = groceryList.filter((grocery) => grocery.category === category)
+  // // const groceryListToDisplay = groceryList.filter((grocery) => category === "All" || grocery.category === category)
+
+  
 
 
   return (
-    <div className="App">
-    <Router>
-      <h1> Grocery Shopping List</h1>
-      <nav>
-        <ul className="linkClass">
-          <li> <Link to='/'> Form - Edit Grocery List </Link> </li>
-          <li> <Link to='categories' > Grocery Category </Link> </li>
-        </ul>
-      </nav>
-
-        <Routes>
-          <Route path='/' element={<Form/>} />
-          <Route path='categories' element={<GroceryCategory />} />
-          <Route path='container' element={<GroceryContainer />} />
-        </Routes>
-      </Router>
+    <div className="app">
+    <div className="sidebar">
+      <button onClick = {handleClick}>Grocery List</button>
+      {show ? <NewGroceryForm handleAddGrocery={handleAddGrocery} /> : null}
     </div>
-  );
-}
+        <GroceryList groceryList={groceryList} handleDeleteGrocery={handleDeleteGrocery}/>
+      </div>
+    );
+  }
+
+
+//     <div className="App">
+//       <h2>My groceryList</h2>
+//       <CategoryFilter 
+//         categories={category}
+//         selectedCategory={category}
+//         onSelectCategory={setCategory}
+//       />
+//       <NewGroceryForm 
+//         categories={category.filter((cat) => cat)}
+//         // categories={category.filter((cat) => cat !== "All")}
+//         // categories={category}
+//         onGroceryFormSubmit={handleAddGrocery}
+//       />
+//       <GroceryList groceryList={groceryListToDisplay} onDeleteGrocery={handleDeleteGrocery}/>
+//     </div>
+//   );
+// }
 
 export default App;
 
@@ -39,52 +70,10 @@ export default App;
 
 
 
-// import React, { useState } from "react";
-// import CategoryFilter from "./CategoryFilter";
-// import NewGroceryForm from "./NewGroceryForm";
-// import GroceryList from "./GroceryList";
-
-// // import { CATEGORIES, GROCERIES } from "../data";
-// // console.log("Here's the data you're working with");
-// // console.log({ CATEGORIES, GROCERIES });
-
-// function App() {
-//   const [groceries, setGroceries] = useState([])
-//   const [category, setCategory] = useState([])
-
-//   useEffect(() => {
-//     fetch("http://localhost:9292/groceries")
-//       .then((r) => r.json())
-//       .then((groceries) => setGroceries(groceries));
-//   }, []);
 
 
 
-//   const handleAddGrocery = (newGrocery) => {
-//     setGroceries([...groceries, newGrocery])
-//   }
 
-//   const handleDeleteGrocery = (groceryToDelete) => {
-//     setGroceries(groceries.filter((grocery) => grocery.id !== groceryToDelete))
-//   }
-
-//   const groceriesToDisplay = groceries.filter((grocery) => category === "All" || grocery.category === category)
-
-//   return (
-//     <div className="App">
-//       <h2>My Groceries</h2>
-//       <CategoryFilter 
-//         categories={category}
-//         selectedCategory={category}
-//         onSelectCategory={setCategory}
-//       />
-//       <NewGroceryForm 
-//         categories={category.filter((cat) => cat !== "All")}
-//         onGroceryFormSubmit={handleAddGrocery}
-//       />
-//       <GroceryList groceries={groceriesToDisplay} onDeleteGrocery={handleDeleteGrocery}/>
-//     </div>
-//   );
-// }
-
-// export default App;
+// import { CATEGORIES, groceryList } from "../data";
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, groceryList });
