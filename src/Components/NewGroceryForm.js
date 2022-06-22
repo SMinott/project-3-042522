755613d476925e5d8c10770d35c93ce7440f0e95
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-06-22 14:32:33 -0400

    added updated groceryform

--- src/Components/NewGroceryForm.js
+++ src/Components/NewGroceryForm.js
@@ -1,81 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Form() {
-  return (
-    <div>
-      <h2>CRUD Activities go here</h2>
-    </div>
-  )
+
+function NewGroceryForm({ handleAddGrocery }) {
+    const [formData, setFormData] = useState({
+        name: "",
+        category: "",
+    })
+
+const handleChange = (event) => {
+    setFormData ({
+      ...formData,
+      [event.target.name]: event.target.value
+    })
+  }
+
+function handleSubmit (e) {
+    e.preventDefault()
+    const groceryData = {
+        name: formData.name,
+        category: formData.category,
+    }
+
+    fetch('http://localhost:9292/groceries', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(groceryData)
+    })
+    .then(res => res.json())
+    .then((data) => {handleAddGrocery(data)
+        setFormData({ 
+            name: "",
+            category: "",
+        })
+    })
 }
 
-export default Form
 
+  return (
+    <form className="new-grocery-form" onSubmit={handleSubmit}>
+        <input 
+        name="name"
+        onChange={handleChange} 
+        placeholder="name" 
+        value={formData.name}
+        />
+      <input
+        name="category" 
+        onChange={handleChange}
+        placeholder="Category"
+        value={formData.category} 
+        />
+      <input type="submit" value="Add to List" />
+    </form>
+  );
+}
 
-
-
-
-// import React, { useState } from "react";
-
-
-// function NewGroceryForm({ categories, onGroceryFormSubmit }) {
-//   const [name, setName] = useState("")
-//   const [category, setCategory] = useState("")
-
-// //   const handleSubmit = (e) => {
-// //     e.preventDefault()
-// //     onGroceryFormSubmit({ text, category })
-// //     setText("")
-// //   }
-
-// function handleSubmit (e) {
-//     e.preventDefault();
-//     const groceryData = {
-//         name: name,
-//         category: category,
-//     }
-//     fetch('http://localhost:9292/groceries', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(groceryData),
-//     })
-//     .then(res => res.json())
-//     .then((newGrocery) => {
-//         onGroceryFormSubmit(newGrocery);
-//     })
-// }
-
-
-//   return (
-//     <form className="new-grocery-form" onSubmit={handleSubmit}>
-//       <label>
-//         Grocery:
-//         <input 
-//         type="text" 
-//         name="text"
-//         value={name}
-//         onChange={(e) => setName(e.target.value)} 
-//         />
-//       </label>
-//       <label>
-//         Category
-//         <select 
-//         name="category" 
-//         value={category} 
-//         onChange={(e) => setCategory(e.target.value)}>
-  
-//           {categories.map((cat) => (
-//             <option key={cat}>{cat}</option>
-//           ))}
-
-//         </select>
-//       </label>
-//       <input type="submit" value="Add grocery" />
-//     </form>
-//   );
-// }
-
-// export default NewGroceryForm;
-
+export default NewGroceryForm;
 
